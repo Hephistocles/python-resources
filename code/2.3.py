a81bfc40912dfc9987cf4ba141d67df49b67ad25
Christopher Little <hephistocles@gmail.com>
2016-07-22 19:07:51 +0100

    Remove unnecessary code files

--- code/2.3.py
+++
@@ -1,8 +0,0 @@
-if (int(input("What did the student get in Geography (as a %)?\n")) > 50):
-	print("They passed!")
-else:
-	print("I'm afraid they failed")
-if (int(input("What did the student get in English (as a %)?\n")) > 50):
-	print("They passed!")
-else:
-	print("I'm afraid they failed")
--- code/3.2.py
+++
@@ -1,6 +0,0 @@
-import os
-end = input("What file type are you interested in?\n")
-dirs = os.listdir("/media/christoph/USB DISK/Python")
-for d in dirs:
-	if (d.endswith(end)):
-		print(d)
